8d585ad0e0e8f76724ef597862aa0b274c0ebe36
Robin <robininispan@gmail.com>
2022-08-01 21:21:46 +0800

    [finished] 95. Finish Creating User Documents

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,19 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
-  return <h1>i am SignIn</h1>;
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+  return (
+    <div>
+      <h1>i am SignIn</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
