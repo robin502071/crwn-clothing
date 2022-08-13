6e782a28f52dc55424542e73397c7d811a58e4c1
robin <robininispan@gmail.com>
2022-08-13 16:20:18 +0800

    [finished] 135. Fixing Clashing Styles、category-preview h2 加上 Link

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -2,14 +2,15 @@ import { Fragment, useContext } from 'react';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 import { CategoriesContext } from '../../contexts/categories.context';
 
-
 const CategoriesPreview = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
     <Fragment>
       {Object.keys(categoriesMap).map((title) => {
         const products = categoriesMap[title];
-        return <CategoryPreview key={title} title={title} products={products} />;
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
       })}
     </Fragment>
   );
