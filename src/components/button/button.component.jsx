4e8e4bed9d3185eed9e6218aaf9e2796a1f31f26
Robin <robininispan@gmail.com>
2022-08-14 19:49:24 +0800

    [finished] 138. Styled-Component - Cart Dropdown

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -10,12 +10,24 @@ export const BUTTON_TYPE_CLASSES = {
   inverted: 'inverted',
 };
 
-const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
-  ({
-    [BUTTON_TYPE_CLASSES.base]: BaseButton,
-    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
-    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
-  }[buttonType]);
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => {
+  // ({
+  //   [BUTTON_TYPE_CLASSES.base]: BaseButton,
+  //   [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+  //   [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  // }[buttonType]);
+
+  switch (buttonType) {
+    case BUTTON_TYPE_CLASSES.base:
+      return BaseButton;
+
+    case BUTTON_TYPE_CLASSES.google:
+      return GoogleSignInButton;
+
+    case BUTTON_TYPE_CLASSES.inverted:
+      return InvertedButton;
+  }
+};
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,32 @@
 import Button from '../button/button.component';
-import './cart-dropdown.styles.scss';
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 import CartItem from '../cart-item/cart-item.component';
 import { useNavigate } from 'react-router-dom';
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from './cart-dropdown.styles';
+
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
   const goToCheckoutPage = () => {
     navigate('/checkout');
   };
+
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty.</EmptyMessage>
+        )}
+      </CartItems>
       <Button onClick={goToCheckoutPage}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
   );
 };
 
