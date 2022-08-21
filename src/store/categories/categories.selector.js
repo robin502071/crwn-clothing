c1695487008514ef1492ce67e8e76eedfb21ab9e
robin <robininispan@gmail.com>
2022-08-21 14:59:16 +0800

    [finished] 162. Reselect Library

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,26 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from 'reselect';
+
+const selectCategoriesReducer = (state) => {
+  console.log('selector 1 fired');
+  return state.categories;
+};
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => {
+    console.log('selector 2 fired');
+    return categoriesSlice.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log('selector 3 fired');
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
