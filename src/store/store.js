73a949a76bc603d0714b1134208813d4b396047d
robin <robininispan@gmail.com>
2022-08-24 14:12:03 +0800

    [updated] 更新 redux-persist with redux toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -2,19 +2,36 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './user/user.slice';
 import { cartReducer } from './cart/cart.slice';
 import { categoriesReducer } from './categories/categories.slice';
+
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+
+import { combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const reducers = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+  categories: categoriesReducer,
+});
 
 const middleWares = [
   process.env.NODE_ENV !== 'production' && logger,
   thunk,
 ].filter(Boolean);
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+  whitelist: ['cart'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-    categories: categoriesReducer,
-  },
+  reducer: persistedReducer,
   middleware: middleWares,
 });
+
+export const persistor = persistStore(store);
